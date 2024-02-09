dda60fe6e3ff2e4886c085a515c78786d6c62448
Colin Torney <colin.torney@glasgow.ac.uk>
2024-02-09 11:49:51 +0000

    fitting with a keras model

--- synthetic_data/data_generation_script.py
+++ synthetic_data/data_generation_script.py
@@ -20,149 +20,110 @@ np.set_printoptions(suppress=True)
 
 ##
 
-L = 50.0 # size of domain
-x = y = np.arange(0,L+1,1)
-var=1.0
+L = 50.0  # size of domain
+x = y = np.arange(0, L+1, 1)
+var = 1.0
 
-for repeat in range(10):
-    for N in [10001, 100001, 1000001]:
+tf.random.set_seed(time())
 
-        for beta in [[[0.5,-0.8]],[[-1.5,-1.8]],[[-1.5,1.8]],[[1.2,1.8]]]:
-            nbObs=N
+nrepeat = 10
 
-            pos_filename  = "pos_N_" + str(nbObs-1) + "_b1_" + str(beta[0][0]) + "_b2_" + str(beta[0][1])+ "_repeat_" + str(repeat) + ".csv"
-            grid_filename  = "cov_N_" + str(nbObs-1) + "_b1_" + str(beta[0][0]) + "_b2_" + str(beta[0][1])+ "_repeat_" + str(repeat) + ".npy"
-            # check if the file exists
-            if os.path.isfile(pos_filename):
-                print("File already exists")
-                continue
-            print("Running simulation for N = ", N, "beta 1 = ", beta[0][0], "beta 2 = ", beta[0][1])
+# Make the environment using a correlated periodic Gaussian random field
+x = y = np.arange(0, L+1, 1)
+xx, yy = np.meshgrid(x, y)
 
-            # Simulating observation from centred model
+positions = np.vstack([xx.ravel(), yy.ravel()]).T
+
+len_scale_1 = 5
+period = np.float64(L)
+periodic_len = (2**0.5)*len_scale_1*np.pi/period
 
 
-            # Make the environment using a correlated periodic Gaussian random field
-            x = y = np.arange(0,L+1,1)
-            xx, yy = np.meshgrid(x,y)
+kernel = tfp.math.psd_kernels.ExpSinSquared(
+    length_scale=periodic_len, period=period)
+gp1 = tfd.GaussianProcess(kernel, positions)
 
-            positions = np.vstack([xx.ravel(), yy.ravel()]).T
-
-            len_scale_1 = 5
-            period = np.float64(L)
-            periodic_len = (2**0.5)*len_scale_1*np.pi/period
+len_scale_2 = 10
+period = np.float64(L)
+periodic_len = (2**0.5)*len_scale_2*np.pi/period
 
 
-            kernel = tfp.math.psd_kernels.ExpSinSquared(length_scale=periodic_len,period=period)
-            gp = tfd.GaussianProcess(kernel, positions)
-
-            samples = gp.sample()
-            cov1 = samples.numpy().reshape(51,51)
+kernel = tfp.math.psd_kernels.ExpSinSquared(
+    length_scale=periodic_len, period=period)
+gp2 = tfd.GaussianProcess(kernel, positions)
 
 
-            len_scale_2 = 10
-            period = np.float64(L)
-            periodic_len = (2**0.5)*len_scale_2*np.pi/period
+@tf.function
+def onestep(xyt, stdt):
+    C = tf.random.normal(shape=(nrepeat, 2), mean=xyt, stddev=stdt[:, None])
+    grid = tf.random.normal(shape=(nrepeat, npts, 2),
+                            mean=C[:, None, :], stddev=stdt[:, None, None])
+
+    zgrid = grid % L
+    cov_xy_end = tfp.math.batch_interp_regular_nd_grid(
+        zgrid, x_min, x_max, cov_tensor, axis=-3)
+    allrsf = tf.reduce_sum(tf.multiply(cov_xy_end, beta_tensor), axis=-1)
+
+    inds = tfd.Categorical(logits=allrsf).sample()
+    locations = tf.gather(grid, inds, batch_dims=1)
+    return locations
 
 
-            kernel = tfp.math.psd_kernels.ExpSinSquared(length_scale=periodic_len,period=period)
-            gp = tfd.GaussianProcess(kernel, positions)
+for nbObs in [10001, 100001, 1000001]:
+    for beta in [[[0.5, -0.8]], [[-1.5, -1.8]], [[-1.5, 1.8]], [[1.2, 1.8]]]:
 
-            samples = gp.sample()
-            cov2 = samples.numpy().reshape(51,51)
+        samples = gp1.sample(nrepeat)
+        cov1 = samples.numpy().reshape(nrepeat, 51, 51)
 
-            ##
+        samples = gp2.sample(nrepeat)
+        cov2 = samples.numpy().reshape(nrepeat, 51, 51)
 
-            # # fig, ax = plt.subplots(1,2,figsize=(8,6))
-            # ax[0].set_title('cov 1')
-            # ax[0].imshow(cov1)
-            # ax[1].set_title('cov 2')
-            # ax[1].imshow(cov2)
-            # plt.show()
+        cov_cube = np.stack((cov1, cov2), axis=1)
 
-            ##
-            cov_cube = np.stack((cov1,cov2))
+        xy0 = [25, 25]  # Initial location
+        npts = 100  # Number of potential endpoints to sample at each time step
+        xy1 = np.zeros((nbObs, nrepeat, 2))
+        xy1[0] = xy0
+        x_min = [0., 0.]
+        x_max = [L, L]
 
+        # Convert the covariates to tensor
+        cov_tensor = tf.transpose(tf.convert_to_tensor(
+            cov_cube, dtype=tf.float32), [0, 3, 2, 1])
+        beta_tensor = tf.convert_to_tensor(beta, dtype=tf.float32)
 
-            # ## Simulate the individuals
+        dt = np.random.normal(loc=1.0, scale=0.1, size=(nrepeat, nbObs-1))
+        ID = np.zeros(nbObs)
 
+        dt = tf.convert_to_tensor(dt, dtype=tf.float32)
+        stds = (0.5*dt)**0.5
+        xy = tf.convert_to_tensor(xy1, dtype=tf.float32)
 
+        for t in tqdm(range(1, nbObs)):
 
+            next_points = onestep(xy[t-1], stds[:, t-1])
+            tindex = tf.repeat(tf.constant(t, dtype=tf.int64), (20))
+            xindex = tf.where(tf.ones_like(next_points))
 
-            allr=np.ones(nbObs) # Vector of radii for movement kernel, or standard deviations
-            cov=cov_cube #Array of covariates (one layer for each covariate)
-            xy0=[25,25] #Initial location
-            npts=100 # Number of potential endpoints to sample at each time step
-            xy1=np.zeros((nbObs,2))
-            xy1[0] = xy0
-            x_min = [0., 0.]
-            x_max = [L, L]
+            xy = tf.tensor_scatter_nd_update(xy, tf.concat(
+                [tindex[:, None], xindex], axis=1), tf.reshape(next_points, (-1)))
 
-            # Convert the covariates to tensor 
-            cov_tensor = tf.transpose(tf.convert_to_tensor(cov_cube,dtype=tf.float32),[2,1,0])
-            beta_tensor = tf.convert_to_tensor(beta,dtype=tf.float32)
-
-            C=np.zeros((2))
-            allrsf=np.zeros(npts)
-            dt = np.zeros(nbObs)
-            ID = np.zeros(nbObs)
-            cid=0
-            t = 1
-            for t in tqdm(range(1,nbObs)):
-                cdt = np.random.normal(loc=1.0,scale=0.1)
-                C[0] = np.random.normal(loc=xy1[t-1,0],scale=allr[t-1]*((0.5*cdt)**0.5))
-                C[1] = np.random.normal(loc=xy1[t-1,1],scale=allr[t-1]*((0.5*cdt)**0.5))
-
-                grid=np.zeros((npts,2))
-                # sample npts points in the circle
-
-                grid[:,0]=np.random.normal(loc=C[0], scale=allr[t-1]*((0.5*cdt)**0.5),size=npts)
-                grid[:,1]=np.random.normal(loc=C[1], scale=allr[t-1]*((0.5*cdt)**0.5),size=npts)               
-
-                # Convert grid to tensor before extracting the values
-                zgrid=tf.convert_to_tensor(grid%L,dtype=tf.float32)
-
-                # calculate the covariates at the step end locations using tensorflow probability
-                cov_xy_end = tfp.math.batch_interp_regular_nd_grid(zgrid, x_min, x_max, cov_tensor, axis=-3)
-
-                # calculate the rsf
-                allrsf= np.array(tf.exp(tf.reduce_sum(tf.multiply(cov_xy_end,beta_tensor),axis=-1)))
-
-                next_point=random.choices(population=grid,weights=allrsf,k=1)
-
-                dt[t-1]=cdt
-                xy1[t]=next_point[0] 
-
-                ID[t]=cid
-
-
-
-            # In[9]:
-
+        for repeat in range(nrepeat):
+            pos_filename = "pos_N_" + str(nbObs-1) + "_b1_" + str(
+                beta[0][0]) + "_b2_" + str(beta[0][1]) + "_repeat_" + str(repeat) + ".csv"
+            grid_filename = "cov_N_" + str(nbObs-1) + "_b1_" + str(
+                beta[0][0]) + "_b2_" + str(beta[0][1]) + "_repeat_" + str(repeat) + ".npy"
 
             # Saving the data to csv
-            hj=pd.DataFrame(xy1)
-            hj.columns=["x","y"]
-            date=pd.DataFrame(dt)
-            date.columns=["time"]
-            Animal_ID=pd.DataFrame(ID)
-            Animal_ID.columns=["ID"]
-
+            hj = pd.DataFrame(xy[:, repeat].numpy())
+            hj.columns = ["x", "y"]
+            date = pd.DataFrame(dt[repeat].numpy())
+            date.columns = ["time"]
+            Animal_ID = pd.DataFrame(ID)
+            Animal_ID.columns = ["ID"]
             # Creating a list of data and merging them by columns
-            simdata=[hj,date,Animal_ID]
-            simdata=pd.concat(simdata,axis=1)
-
-            #Saving the data
-            simdata.to_csv(pos_filename,index=None)
-            np.save(grid_filename,cov_cube)
-
-
-
-
-
-
-
-
-
-
-
-
+            simdata = [hj, date, Animal_ID]
+            simdata = pd.concat(simdata, axis=1)
+            # Saving the data
+            simdata.to_csv(pos_filename, index=None)
+            np.save(grid_filename, cov_cube[repeat])
---
+++ synthetic_inference/fit_model.py
@@ -0,0 +1,184 @@
+
+##
+
+# import modules
+import os
+import numpy as np
+import random
+import math
+import pandas as pd
+import scipy.stats as stats
+# import seaborn as sns
+import sys
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from time import time
+# import arviz as az
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+tfb = tfp.bijectors
+np.set_printoptions(suppress=True)
+
+# from mssf import stepSelectionVI
+
+
+# Load data
+
+
+nbObs = 10001  # nbObs Number of observations
+beta = np.array([[0.5, -0.8]])  # Vector of resource selection coefficients
+L = 50.0
+
+pos_filename = "pos_N_" + \
+    str(nbObs-1) + "_b1_" + str(beta[0][0]) + \
+    "_b2_" + str(beta[0][1]) + "_repeat_0.csv"
+grid_filename = "cov_N_" + \
+    str(nbObs-1) + "_b1_" + str(beta[0][0]) + \
+    "_b2_" + str(beta[0][1]) + "_repeat_0.npy"
+
+
+# get home directory from os
+home_dir = os.path.expanduser("~")
+data_dir = os.path.join(home_dir, "workspace", "mssf-vi")
+
+
+df = pd.read_csv(data_dir + "/synthetic_data/" + pos_filename)
+
+xy1 = df[["x", "y"]].values  # ,["y"]].values
+dt = df["time"].values  # ,["y"]].values
+ID = df["ID"].values  # ,["y"]].values
+# df
+
+# print(xy1.shape)
+# print(cov_cube.shape)
+cov_cube = np.load(data_dir + "/synthetic_data/" + grid_filename)
+cov_cube = cov_cube[0]
+print(cov_cube.shape)
+
+##
+
+# ## Process to convert to steps
+
+# In[3]:
+
+
+# preprocessing steps convert to tensors and handle the change of ID, start/end points and time between fixes
+start_points = []
+end_points = []
+step_times = []
+for i in np.unique(ID):
+    cxy = xy1[ID == i]  # [:1024*100+1]
+    cdt = dt[ID == i]  # [:1024*100+1]
+    if cdt.shape[0] < 2:
+        continue
+    start_points.append(cxy[:-1])
+    end_points.append(cxy[1:])
+    step_times.append(np.atleast_2d(cdt[:-1]).T)
+
+
+indexes = np.arange(np.vstack(start_points).shape[0])
+np.random.shuffle(indexes)
+start_points = tf.convert_to_tensor(
+    np.vstack(start_points)[indexes], dtype=tf.float32)
+end_points = tf.convert_to_tensor(
+    np.vstack(end_points)[indexes], dtype=tf.float32)
+step_times = tf.convert_to_tensor(
+    np.vstack(step_times)[indexes], dtype=tf.float32)
+
+
+cov_tensor = tf.transpose(tf.convert_to_tensor(
+    cov_cube, dtype=tf.float32), [2, 1, 0])
+
+x_ref_min = [0., 0.]
+x_ref_max = [L, L]
+
+##
+
+# ## Create the instance of the model and set up the training
+
+# In[4]:
+
+
+ssf = stepSelectionVI(2, cov_tensor, move_std=1.00,
+                      L=50.0, n_gh_points=3, n_vi_samples=64)
+
+
+##
+# In[5]:
+
+
+# set up the dataset and optimizer
+batch_size = 1024
+train_dataset = tf.data.Dataset.from_tensor_slices(
+    (start_points, end_points, step_times))
+train_dataset = train_dataset.shuffle(
+    buffer_size=64).batch(batch_size, drop_remainder=True)
+# train_dataset = train_dataset.batch(batch_size,drop_remainder=True)
+
+kl_weight = batch_size/start_points.shape[0]
+# optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001)
+
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    0.1, decay_steps=100, decay_rate=0.9, staircase=True)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+ssf.compile(optimizer=optimizer, loss_weights=kl_weight)
+##
+# In[6]:
+
+
+def plot_fit():  # data_batch
+    print('move std ' + str(ssf.move_std.numpy()) + 'betas ' +
+          str(ssf.beta_mean.numpy()) + ' stds: ', str(ssf.beta_std.numpy().flatten()))
+
+    b1mean = ssf.beta_mean.numpy()[0]
+    b1scale = ssf.beta_std.numpy()[0, 0, 0]
+    b2mean = ssf.beta_mean.numpy()[1]
+    b2scale = ssf.beta_std.numpy()[0, 1, 1]
+
+    x = np.arange(b1mean-b1scale*5, b1mean+b1scale*5, 0.001)
+    y = tfp.distributions.Normal(loc=b1mean, scale=b1scale).prob(x)
+
+    plt.plot(x, y, c='C0')
+    plt.hist(chain1[:, 0, 0], bins=20, density=True, color='C1')
+    plt.fill_between(x, y, color='C0', alpha=0.5)
+    plt.axvline(0.5, c='C1')
+    # ax[0].set_ylim(0,y.numpy().max()*1.2)
+    plt.show()
+
+    x = np.arange(b2mean-b2scale*5, b2mean+b2scale*5, 0.001)
+    y = tfp.distributions.Normal(loc=b2mean, scale=b2scale).prob(x)
+
+    plt.plot(x, y, c='C0')
+    plt.fill_between(x, y, color='C0', alpha=0.5)
+
+    plt.axvline(-0.8, c='C1')
+    # plt.ylim(0,y.numpy().max()*1.2)
+    # plt.savefig('1millionpoints.png')
+    plt.show()
+
+
+##
+
+ssf.fit(train_dataset, epochs=5)
+# calculate the correlation between chain[:,0,0] and chain[:,0,1]
+# cor = np.corrcoef(chain1[:,0,0],chain1[:,0,1])[0,1]
+##
+
+epochs = range(5)
+
+for epoch in epochs:
+    epoch_loss = 0.0
+    for data_batch in tqdm(train_dataset):
+        with tf.GradientTape() as tape:
+            loss = ssf.variational_loss(*data_batch, kl_weight)
+        epoch_loss += np.squeeze(loss.numpy())
+        gradients = tape.gradient(loss, ssf.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, ssf.trainable_variables))
+        # break
+    # plot_fit()
+    # if epoch % 1 == 0:
+        # print('Epoch ' + str(epoch) + ' complete. Loss: ', epoch_loss)
+        # print('Epoch ' + str(epoch) +' betas ' + str(ssf2.beta_mean.numpy()) + ' stds: ', str(ssf2.beta_std.numpy().flatten()))
