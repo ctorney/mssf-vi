95aeeeeb5f4fb0e13a1ad825b132583baf74eb1f
Colin Torney <colin.torney@glasgow.ac.uk>
2024-02-16 11:19:29 +0000

    clean up

--- synthetic_inference/fit_model.py
+++ synthetic_inference/fit_model.py
@@ -1,5 +1,4 @@
 # import modules
-from mssf_model import stepSelectionVI
 import os
 import numpy as np
 import random
@@ -19,6 +18,8 @@ tfb = tfp.bijectors
 np.set_printoptions(suppress=True)
 sys.path.append(".")
 
+from mssf_model import stepSelectionVI
+from mssf_model import ConvergenceCallback
 
 # Load data
 
@@ -27,12 +28,8 @@ nbObs = 10001  # nbObs Number of observations
 beta = np.array([[0.5, -0.8]])  # Vector of resource selection coefficients
 L = 50.0
 
-pos_filename = "pos_N_" + \
-    str(nbObs-1) + "_b1_" + str(beta[0][0]) + \
-    "_b2_" + str(beta[0][1]) + "_repeat_0.csv"
-grid_filename = "cov_N_" + \
-    str(nbObs-1) + "_b1_" + str(beta[0][0]) + \
-    "_b2_" + str(beta[0][1]) + "_repeat_0.npy"
+pos_filename = "pos_N_" + str(nbObs-1) + "_b1_" + str(beta[0][0]) + "_b2_" + str(beta[0][1]) + "_repeat_0.csv"
+grid_filename = "cov_N_" + str(nbObs-1) + "_b1_" + str(beta[0][0]) + "_b2_" + str(beta[0][1]) + "_repeat_0.npy"
 
 
 # get home directory from os
@@ -42,23 +39,16 @@ data_dir = os.path.join(home_dir, "workspace", "mssf-vi")
 
 df = pd.read_csv(data_dir + "/synthetic_data/" + pos_filename)
 
-xy1 = df[["x", "y"]].values  # ,["y"]].values
-dt = df["time"].values  # ,["y"]].values
-ID = df["ID"].values  # ,["y"]].values
-# df
+xy1 = df[["x", "y"]].values 
+dt = df["time"].values  
+ID = df["ID"].values  
 
-# print(xy1.shape)
-# print(cov_cube.shape)
 cov_cube = np.load(data_dir + "/synthetic_data/" + grid_filename)
 cov_cube = cov_cube[0]
-print(cov_cube.shape)
 
 ##
 
-# ## Process to convert to steps
-
-# In[3]:
-
+# Process to convert to steps
 
 # preprocessing steps convert to tensors and handle the change of ID, start/end points and time between fixes
 start_points = []
@@ -76,134 +66,37 @@ for i in np.unique(ID):
 
 indexes = np.arange(np.vstack(start_points).shape[0])
 np.random.shuffle(indexes)
-start_points = tf.convert_to_tensor(
-    np.vstack(start_points)[indexes], dtype=tf.float32)
-end_points = tf.convert_to_tensor(
-    np.vstack(end_points)[indexes], dtype=tf.float32)
-step_times = tf.convert_to_tensor(
-    np.vstack(step_times)[indexes], dtype=tf.float32)
+start_points = tf.convert_to_tensor(np.vstack(start_points)[indexes], dtype=tf.float32)
+end_points = tf.convert_to_tensor(np.vstack(end_points)[indexes], dtype=tf.float32)
+step_times = tf.convert_to_tensor(np.vstack(step_times)[indexes], dtype=tf.float32)
 
-
-cov_tensor = tf.transpose(tf.convert_to_tensor(
-    cov_cube, dtype=tf.float32), [2, 1, 0])
+cov_tensor = tf.transpose(tf.convert_to_tensor(cov_cube, dtype=tf.float32), [2, 1, 0])
 
 x_ref_min = [0., 0.]
 x_ref_max = [L, L]
 
 ##
 
-# ## Create the instance of the model and set up the training
+# Create the instance of the model and set up the training
 
-# In[4]:
-
-
-ssf = stepSelectionVI(2, cov_tensor, move_std=2.00,
-                      L=50.0, n_gh_points=3, n_gh_points_vi=5)
-
+ssf = stepSelectionVI(2, cov_tensor, move_std=2.00,L=50.0, n_gh_points=3, n_gh_points_vi=5)
 
 # set up the dataset and optimizer
 batch_size = 1000
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    (start_points, end_points, step_times))
-# train_dataset = train_dataset.batch(batch_size, drop_remainder=True)
-# train_dataset = train_dataset.shuffle(
-#     buffer_size=100000).batch(batch_size, drop_remainder=True)
+train_dataset = tf.data.Dataset.from_tensor_slices((start_points, end_points, step_times))
 train_dataset = train_dataset.batch(batch_size,drop_remainder=True)
 
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1, use_ema=True, ema_overwrite_frequency=10) 
+
 kl_weight = batch_size/start_points.shape[0]
-optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.1)
-
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    0.1, decay_steps=10, decay_rate=0.9, staircase=False)
-##
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.1, use_ema=True, ema_overwrite_frequency=10, ema_momentum=0.99)
-                                     #, amsgrad=True, beta_1 = 0.99)
-
-# optimizer = tfp.optimizer.VariationalSGD(batch_size, total_num_examples=start_points.shape[0], max_learning_rate=1.0, burnin=20, burnin_max_learning_rate=0.1)
-# clip value is wrong because it's bouncing back and forth
-# optimizer = tf.keras.optimizers.SGD(learning_rate=0.01,clipvalue=10.0)  # , beta_1 = 0.5)
 ssf.compile(optimizer=optimizer, loss_weights=kl_weight)
 
-# add the diff norm metric
-#https://github.com/pymc-devs/pymc/blob/0d8ddbace0056bdeae01d706e52ac11baf2cf3f3/pymc/variational/callbacks.py#L44 
-##
-early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor='loss', patience=100) #, min_delta=0.001)
-convergence_callback = ConvergenceCallback(1e-3)
-ssf.fit(train_dataset, epochs=500, callbacks=[convergence_callback])
-##
-# In[6]:
+convergence_callback = ConvergenceCallback(threshold=1e-3)
 
-chain1 = np.load('chain1.npy')
-
-
-def plot_fit1():  # data_batch
-    print('move std ' + str(ssf.move_std.numpy()) + 'betas ' +
-          str(ssf.beta_mean.numpy()) + ' stds: ', str(ssf.beta_std.numpy().flatten()))
-
-    b1mean = ssf.beta_mean.numpy()[0]
-    b1scale = ssf.beta_std.numpy()[0, 0]
-    b2mean = ssf.beta_mean.numpy()[1]
-    b2scale = ssf.beta_std.numpy()[0, 1]
-
-    x = np.arange(b1mean-b1scale*5, b1mean+b1scale*5, 0.001)
-    y = tfp.distributions.Normal(loc=b1mean, scale=b1scale).prob(x)
-
-    plt.plot(x, y, c='C0')
-    plt.hist(chain1[:, 0, 0], bins=10, density=True, color='C1')
-    plt.fill_between(x, y, color='C0', alpha=0.5)
-    plt.axvline(0.5, c='C1')
-    # ax[0].set_ylim(0,y.numpy().max()*1.2)
-    plt.show()
-    return
-def plot_fit2():  # data_batch
-    print('move std ' + str(ssf.move_std.numpy()) + 'betas ' +
-          str(ssf.beta_mean.numpy()) + ' stds: ', str(ssf.beta_std.numpy().flatten()))
-
-    b1mean = ssf.beta_mean.numpy()[0]
-    b1scale = ssf.beta_std.numpy()[0, 0]
-    b2mean = ssf.beta_mean.numpy()[1]
-    b2scale = ssf.beta_std.numpy()[0, 1]
-
-    x = np.arange(b2mean-b2scale*5, b2mean+b2scale*5, 0.001)
-    y = tfp.distributions.Normal(loc=b2mean, scale=b2scale).prob(x)
-
-    plt.plot(x, y, c='C0')
-    plt.hist(chain1[:, 0, 1], bins=20, density=True, color='C1')
-    plt.fill_between(x, y, color='C0', alpha=0.5)
-
-    plt.axvline(-0.8, c='C1')
-    # plt.ylim(0,y.numpy().max()*1.2)
-    # plt.savefig('1millionpoints.png')
-    plt.show()
 
 
 ##
-plot_fit()
+max_epochs = 500
+ssf.fit(train_dataset, epochs=max_epochs, callbacks=[convergence_callback])
 ##
-# add early stopping
-
-early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor='loss', patience=3, min_delta=0.1)
-
-ssf.fit(train_dataset, epochs=50)  # , callbacks=[early_stopping])
-# calculate the correlation between chain[:,0,0] and chain[:,0,1]
-# cor = np.corrcoef(chain1[:,0,0],chain1[:,0,1])[0,1]
-##
-
-epochs = range(5)
-
-for epoch in epochs:
-    epoch_loss = 0.0
-    for data_batch in tqdm(train_dataset):
-        with tf.GradientTape() as tape:
-            loss = ssf.variational_loss(*data_batch, kl_weight)
-        epoch_loss += np.squeeze(loss.numpy())
-        gradients = tape.gradient(loss, ssf.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, ssf.trainable_variables))
-        # break
-    # plot_fit()
-    # if epoch % 1 == 0:
-        # print('Epoch ' + str(epoch) + ' complete. Loss: ', epoch_loss)
-        # print('Epoch ' + str(epoch) +' betas ' + str(ssf2.beta_mean.numpy()) + ' stds: ', str(ssf2.beta_std.numpy().flatten()))
--- synthetic_inference/mssf_model.py
+++ synthetic_inference/mssf_model.py
@@ -20,13 +20,12 @@ class ConvergenceCallback(tf.keras.callbacks.Callback):
 
         current_variables = np.concatenate([v.numpy().flatten() for v in self.model.trainable_variables])
 
-        diff = current_variables - self.previous_variables  # type: ignore
+        diff = current_variables - self.previous_variables 
         relative_diff = (np.abs(diff)) / (np.abs(self.previous_variables) + self.eps)
 
         self.previous_variables = current_variables
 
         norm = np.linalg.norm(relative_diff, np.inf)
-        print("\nConvergence diagnostic: ", norm, "\n")
         if norm < self.threshold:
             self.stopped_epoch = epoch
             self.model.stop_training = True
@@ -34,51 +33,9 @@ class ConvergenceCallback(tf.keras.callbacks.Callback):
 
 
 
-class BinaryTruePositives(tf.keras.metrics.Metric):
-
-      def __init__(self, name='binary_true_positives', tv=None, **kwargs):
-        super(BinaryTruePositives, self).__init__(name=name, **kwargs)
-        self.true_positives = self.add_weight(name='tp', initializer='zeros')
-        self.gradients = [self.add_weight(name=v.name,shape=v.shape, initializer='ones') for v in tv]
-        self.decay = 1. - 1. / tf.cast(10, np.float32)
-
-      def update_state(self, gradients):
-
-        grad_inner_product = sum(tf.reduce_sum(g1 * g2)/(tf.norm(g1)*tf.norm(g2))
-                                  for g1, g2 in zip(self.gradients, gradients))
-
-
-        # grad_inner_product = tf.reduce_sum(self.gradients[0][0] * gradients[0][0])
-        # tf.print('\nvalues that go into it\n', self.gradients[0][0], gradients[0][0], output_stream=sys.stdout)
-        self.true_positives.assign(self.decay*self.true_positives + (1-self.decay)*grad_inner_product)
-        # self.true_positives.assign(grad_inner_product)
-        # tf.print('\n', self.true_positives, grad_inner_product, output_stream=sys.stdout)
-        for g1, g2 in zip(self.gradients, gradients):
-          # tf.print('\n',g1, g2, output_stream=sys.stdout)
-          g1.assign(g2)
-        # # self.moving_product = (
-        #     # grad_inner_product + self.decay * (self.moving_product - grad_inner_product))
-
-        # self.previous_grads = gradients
-        # y_true = tf.cast(y_true, tf.bool)
-        # y_pred = tf.cast(y_pred, tf.bool)
-        #
-        # values = tf.logical_and(tf.equal(y_true, True), tf.equal(y_pred, True))
-        # values = tf.cast(values, self.dtype)
-        # if sample_weight is not None:
-        #   sample_weight = tf.cast(sample_weight, self.dtype)
-        #   sample_weight = tf.broadcast_to(sample_weight, values.shape)
-        #   values = tf.multiply(values, sample_weight)
-        # self.true_positives.assign_add(tf.reduce_sum(values))
-
-      def result(self):
-        return self.true_positives
-
 # A periodic version for working with simulated data on a torus
-
-
 class stepSelectionVI(tf.keras.Model):
-    def __init__(self, n_covars, cov_tensor, move_std=1.0, L=1.0,  prior_mean=None, prior_std=None, n_gh_points=4, n_gh_points_vi=4, window_size=10):
+    def __init__(self, n_covars, cov_tensor, move_std=1.0, L=1.0,  prior_mean=None, prior_std=None, n_gh_points=4, n_gh_points_vi=4):
 
         super().__init__()
 
@@ -97,8 +54,7 @@ class stepSelectionVI(tf.keras.Model):
         xi, wi = np.polynomial.hermite.hermgauss(n_gh_points)
 
         ghx, ghy = np.meshgrid(xi, xi)
-        self.gh_grid = tf.convert_to_tensor(
-            np.stack([ghx, ghy], axis=2).astype(np.float32)) 
+        self.gh_grid = tf.convert_to_tensor(np.stack([ghx, ghy], axis=2).astype(np.float32)) 
 
         ghx, ghy = np.meshgrid(wi, wi)
 
@@ -111,17 +67,8 @@ class stepSelectionVI(tf.keras.Model):
         xi, wi = np.polynomial.hermite.hermgauss(n_gh_points_vi)
 
         vi_grid = np.meshgrid(*[xi]*n_covars)
-        self.gh_grid_vi = tf.convert_to_tensor(
-            np.stack(vi_grid, axis=-1).astype(np.float32))
-
+        self.gh_grid_vi = tf.convert_to_tensor(np.stack(vi_grid, axis=-1).astype(np.float32))
         self.gh_grid_vi = tf.reshape(self.gh_grid_vi, (-1, n_covars)) * (2**0.5)
-        # ghx, ghy = np.meshgrid(wi, wi)
-        #
-        # n_gh_points_vi = 4
-        # n_covars = 3
-        # index_grid = np.meshgrid(*[np.arange(n_gh_points_vi)]*n_covars)
-
-        # index_grid = np.stack(index_grid, axis=-1).astype(np.float32)
 
         weight_grid = np.meshgrid(*[wi]*n_covars)
         weight_grid = np.stack(weight_grid, axis=-1).astype(np.float32) / (np.pi**0.5)
@@ -130,19 +77,14 @@ class stepSelectionVI(tf.keras.Model):
 
         self.gh_grid_weights_vi = tf.reshape(gh_grid_weights_vi, (-1)) 
 
-        #
-
-
+        # set the initial variational parameters - use zero mean and 0.1 std 
         self.beta_mean = tf.Variable(np.zeros((n_covars)), dtype=tf.float32)
 
-        self.beta_std = tfp.util.TransformedVariable(
-            [np.ones(n_covars, dtype=np.float32)/10.0], tfp.bijectors.Softplus(), dtype=tf.float32)
+        self.beta_std = tfp.util.TransformedVariable([np.ones(n_covars, dtype=np.float32)/10.0], tfp.bijectors.Softplus(), dtype=tf.float32)
 
-        self.variational_posterior = tfp.distributions.MultivariateNormalDiag(
-            loc=self.beta_mean, scale_diag=self.beta_std)
+        self.variational_posterior = tfp.distributions.MultivariateNormalDiag(loc=self.beta_mean, scale_diag=self.beta_std)
 
-        self.move_std = tfp.util.TransformedVariable(
-            move_std, tfp.bijectors.Softplus(), dtype=tf.float32, trainable=True)
+        self.move_std = tfp.util.TransformedVariable(move_std, tfp.bijectors.Softplus(), dtype=tf.float32, trainable=True)
 
         # set default prior to be N(0,10)
         if prior_mean is None:
@@ -151,18 +93,7 @@ class stepSelectionVI(tf.keras.Model):
             prior_std = 10.0*np.ones((n_covars))
         prior_mean = tf.constant(prior_mean, dtype=tf.float32)
         prior_std = tf.constant(prior_std, dtype=tf.float32)
-        self.prior = tfp.distributions.MultivariateNormalDiag(
-            loc=prior_mean, scale_diag=prior_std)
-
-        self.window_size = window_size
-
-        self.previous_grads = None
-        self.converged = False
-
-        # self.previous_grads = [tf.Variable(tf.ones_like(v), trainable=False)
-                               # for v in self.trainable_variables]
-        self.decay = 1. - 1. / tf.cast(self.window_size, np.float32)
-        self.convergence_tracker = BinaryTruePositives(name="conv", tv=self.trainable_variables)
+        self.prior = tfp.distributions.MultivariateNormalDiag(loc=prior_mean, scale_diag=prior_std)
 
     def call(self):
         return self.variational_posterior
@@ -181,44 +112,28 @@ class stepSelectionVI(tf.keras.Model):
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
         self.loss_tracker.update_state(loss)
-        self.convergence_tracker.update_state(gradients)
 
-        # grad_inner_product = sum(tf.reduce_sum(g1 * g2)
-                                 # for g1, g2 in zip(self.previous_grads, gradients))
-        # # self.moving_product = (
-        #     # grad_inner_product + self.decay * (self.moving_product - grad_inner_product))
-
-        # self.previous_grads = gradients
-
-        return {"loss": self.loss_tracker.result(), "converged": self.convergence_tracker.result(), "beta std": self.beta_std[0][0], "beta mean": self.beta_mean[0]}
+        return {"loss": self.loss_tracker.result()} 
 
     @tf.function
     def variational_loss(self, start_points_batch, end_points_batch, step_times_batch, kl_weight=1.0):
 
-        # beta_samples = self.variational_posterior.sample(self.n_vi_samples)[
-            # :, 0, :]
-
         beta_values = self.beta_mean[None] + self.beta_std * self.gh_grid_vi 
         log_likelihood = self.log_likelhood(beta_values, start_points_batch, end_points_batch, step_times_batch)
         elogp = tf.reduce_sum(self.gh_grid_weights_vi*log_likelihood)
-        penalty = kl_weight * \
-            tfp.distributions.kl_divergence(
-                self.variational_posterior, self.prior)
+        penalty = kl_weight * tfp.distributions.kl_divergence(self.variational_posterior, self.prior)
 
         return -elogp+penalty
 
-    # fast vectorised tf code
 
     @tf.function
     def log_likelhood(self, beta_params, start_points, end_points, step_times):
         # calculate the covariates at the step end locations using tensorflow probability
         env_points = tf.math.mod(end_points, self.L)
 
-        cov_xy_end = tfp.math.batch_interp_regular_nd_grid(
-            env_points, self.x_ref_min, self.x_ref_max, self.cov_tensor, axis=-3)
+        cov_xy_end = tfp.math.batch_interp_regular_nd_grid(env_points, self.x_ref_min, self.x_ref_max, self.cov_tensor, axis=-3)
         # calculate the rsf
-        rsf_points_end = tf.exp(tf.reduce_sum(tf.multiply(tf.expand_dims(
-            cov_xy_end, 0), tf.expand_dims(beta_params, 1)), axis=-1))
+        rsf_points_end = tf.exp(tf.reduce_sum(tf.multiply(tf.expand_dims(cov_xy_end, 0), tf.expand_dims(beta_params, 1)), axis=-1))
 
         # sigma_var = 1.0 ## just for now - needs to be optimised too
         sigmas = tf.math.sqrt(self.move_std * 0.5*step_times)
@@ -231,13 +146,10 @@ class stepSelectionVI(tf.keras.Model):
 
             # shape is numvisamples, num steps, ghx,ghy, 1xnc matrix of coefficients
             betas = tf.reshape(beta_params, (-1, 1, 1, 1, 1, self.n_covars))
-            gh_points = tf.math.mod(
-                means+(2**0.5)*half_sigma*self.gh_grid, self.L)
-            cov_gh_points = tfp.math.batch_interp_regular_nd_grid(
-                gh_points, self.x_ref_min, self.x_ref_max, self.cov_tensor, axis=-3)
+            gh_points = tf.math.mod(means+(2**0.5)*half_sigma*self.gh_grid, self.L)
+            cov_gh_points = tfp.math.batch_interp_regular_nd_grid(gh_points, self.x_ref_min, self.x_ref_max, self.cov_tensor, axis=-3)
             # need to add a dimension at the start to broadcast with num samples
-            cov_gh_points = tf.expand_dims(
-                tf.expand_dims(cov_gh_points, -1), 0)
+            cov_gh_points = tf.expand_dims(tf.expand_dims(cov_gh_points, -1), 0)
 
             rsf = tf.exp(tf.matmul(betas, cov_gh_points)[..., 0, 0])
 
@@ -255,30 +167,19 @@ class stepSelectionVI(tf.keras.Model):
 
         mid_points = mean_c + half_sigma*tf.expand_dims(self.gh_grid, 2)
 
-        inv_int_z = tf.math.pow(tf.map_fn(fn, tf.reshape(
-            mid_points, (self.n_gh_points*self.n_gh_points, -1, 2)), parallel_iterations=1), -1)
+        inv_int_z = tf.math.pow(tf.map_fn(fn, tf.reshape( mid_points, (self.n_gh_points*self.n_gh_points, -1, 2)), parallel_iterations=1), -1)
 
-        inv_int_z = tf.reshape(inv_int_z, (self.n_gh_points, self.n_gh_points, tf.shape(
-            inv_int_z)[1], tf.shape(inv_int_z)[2]))
+        inv_int_z = tf.reshape(inv_int_z, (self.n_gh_points, self.n_gh_points, tf.shape(inv_int_z)[1], tf.shape(inv_int_z)[2]))
 
         # inv_int_z is now shape (ghx,ghy,num vi samples, num steps)
-        ysum = tf.reduce_sum(inv_int_z*tf.reshape(self.grid_gh_weights,
-                             (self.n_gh_points, self.n_gh_points, 1, 1))/(np.pi**0.5), axis=0)
-        xsum = tf.reduce_sum(
-            ysum*tf.reshape(self.gh_weights, (self.n_gh_points, 1, 1))/(np.pi**0.5), axis=0)
+        ysum = tf.reduce_sum(inv_int_z*tf.reshape(self.grid_gh_weights, (self.n_gh_points, self.n_gh_points, 1, 1))/(np.pi**0.5), axis=0)
+        xsum = tf.reduce_sum( ysum*tf.reshape(self.gh_weights, (self.n_gh_points, 1, 1))/(np.pi**0.5), axis=0)
 
-        step_log_prob = tfp.distributions.Independent(tfp.distributions.Normal(
-            loc=start_points, scale=(2**0.5)*sigmas), reinterpreted_batch_ndims=1).log_prob(end_points)
+        step_log_prob = tfp.distributions.Independent(tfp.distributions.Normal( loc=start_points, scale=(2**0.5)*sigmas), reinterpreted_batch_ndims=1).log_prob(end_points)
 
-        log_prob = tf.math.log(rsf_points_end) + \
-            tf.math.log(xsum) + step_log_prob
+        log_prob = tf.math.log(rsf_points_end) + tf.math.log(xsum) + step_log_prob
         return tf.math.reduce_sum(log_prob, axis=-1)
 
     @property
     def metrics(self):
-        # We list our `Metric` objects here so that `reset_states()` can be
-        # called automatically at the start of each epoch
-        # or at the start of `evaluate()`.
-        # If you don't implement this property, you have to call
-        # `reset_states()` yourself at the time of your choosing.
         return [self.loss_tracker]
